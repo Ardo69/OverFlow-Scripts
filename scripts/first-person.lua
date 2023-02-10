a6870e529cb50717e905f5ebb9419f23fdf250ee
OverFlow69 <98959759+OverFlow69@users.noreply.github.com>
2023-02-10 11:56:44 +0300

    first script(s)?!?!?!!?!

---
+++ scripts/first-person.lua
@@ -0,0 +1,44 @@
+local player = game.Players.LocalPlayer
+
+player.CameraMode = Enum.CameraMode.LockFirstPerson
+--locked to first person
+
+local plr = game.Players.LocalPlayer;
+local mouse = plr:GetMouse();
+--locked in the cursors
+
+local player = game.Players.LocalPlayer
+local char = player.Character
+local RunService = game:GetService("RunService")
+
+char.Humanoid.CameraOffset = Vector3.new(0, 0, -1)
+--adds the haha funny vector
+
+for i, v in pairs(char:GetChildren()) do
+    if v:IsA("BasePart") and v.Name ~= "Head" then
+
+        v:GetPropertyChangedSignal("LocalTransparencyModifier"):Connect(function()
+            v.LocalTransparencyModifier = v.Transparency
+        end)
+
+        v.LocalTransparencyModifier = v.Transparency
+
+    end
+end
+--changes the transparency of the head and the accessories
+
+RunService.RenderStepped:Connect(function(step)
+    local ray = Ray.new(char.Head.Position, ((char.Head.CFrame + char.Head.CFrame.LookVector * 2) - char.Head.Position).Position.Unit)
+    --haha yes i will use something deprecated
+    local ignoreList = char:GetChildren()
+
+    local hit, pos = game.Workspace:FindPartOnRayWithIgnoreList(ray, ignoreList)
+    --ignores some rays
+
+    if hit then
+        char.Humanoid.CameraOffset = Vector3.new(0, 0, -(char.Head.Position - pos).magnitude)
+    else
+        char.Humanoid.CameraOffset = Vector3.new(0, 0, -1)
+    end
+    --camera shit
+end)
